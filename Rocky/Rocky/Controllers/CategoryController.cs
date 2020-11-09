a7bd5ac75ea99de5b17e2833cc0c08823d7b18ff
Eduardo <edalvarez2015@gmail.com>
2020-11-08 21:35:40 -0600

    Added migrations to db

---
+++ Rocky/Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+    }
+}
---
+++ Rocky/Rocky/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DisplayName { get; set; }
+        
+        
+        
+        
+        
+        
+        
+    }
+}
