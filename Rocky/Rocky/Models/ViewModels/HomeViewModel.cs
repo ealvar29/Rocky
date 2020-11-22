12ec192331163d8c4615dee8b0729692747aff2d
Eduardo <edalvarez2015@gmail.com>
2020-11-22 00:03:49 -0600

    Configuring Home / Index to display all products

--- Rocky/Rocky/Models/ViewModels/HomeViewModel.cs
+++ Rocky/Rocky/Models/ViewModels/HomeViewModel.cs
@@ -6,7 +6,6 @@ namespace Rocky.Models.ViewModels
     public class HomeViewModel
     {
         public IEnumerable <Product> Products { get; set; }
-
         public IEnumerable <Category> Categories { get; set; }
 
         public IEnumerable <ApplicationType> Applications { get; set; }
